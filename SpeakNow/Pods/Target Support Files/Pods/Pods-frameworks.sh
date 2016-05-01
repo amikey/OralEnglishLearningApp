915b9df9688f54e1f573166cf1caa581e4a0859c
称一称(air) <yicheng.fzu@gmail.com>
2016-05-01 16:31:53 +0800

    pod install

--- SpeakNow/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ SpeakNow/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -89,6 +89,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/KVNProgress.framework"
   install_framework "Pods/KeychainSwift.framework"
   install_framework "Pods/Material.framework"
+  install_framework "Pods/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Alamofire.framework"
@@ -96,4 +97,5 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/KVNProgress.framework"
   install_framework "Pods/KeychainSwift.framework"
   install_framework "Pods/Material.framework"
+  install_framework "Pods/SwiftyJSON.framework"
 fi
